26f923a4b9e82eaed8928a156c33493167745006
AYUSH SINGH <ayushsinghpanchet07@gmail.com>
2025-07-11 02:08:11 +0530

    added signup endpoint and some changes in dc

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,16 +3,34 @@ import jwt from "jsonwebtoken";
 import { middleware } from "./middleware";
 import {CreateUserSchema, SigninSchema, CreateRoomSchema} from "@repo/common/types"
 import {JWT_SECRET} from "@repo/backend-common/config";
+import {prismaClient} from "@repo/db/client";
 const app = express();
 app.use(express.json());
 
-app.post("/signup", (req,res) => {
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success){
+app.post("/signup", async (req,res) => {
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if(!parsedData.success){
+        console.log(parsedData.error);
         res.json({
             message : "Incorrect inputs"
         })
         return;
+    }
+    try{
+        const user = await prismaClient.user.create({
+        data:{
+            email: parsedData.data?.username,
+            password: parsedData.data.password,
+            name: parsedData.data.name
+        }
+    })
+    res.json({
+        userId : user.id
+    })
+    }catch(e){
+        res.status(411).json({
+            message: "User already exist with this email"
+        })
     }
 })
 
@@ -24,4 +42,4 @@ app.post("/room", middleware, (req,res) => {
     
 })
 
-app.listen(3002);
+app.listen(3001);
