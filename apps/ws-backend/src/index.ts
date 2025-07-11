e75489f8f552f910ba90522f44949d11590e0646
AYUSH SINGH <ayushsinghpanchet07@gmail.com>
2025-07-11 14:14:21 +0530

    added join_room logic and improvment in ws-layer

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,5 +1,5 @@
 import { WebSocket, WebSocketServer } from "ws";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import {JWT_SECRET} from "@repo/backend-common/config";
 import {prismaClient} from "@repo/db/client";
 
@@ -27,8 +27,6 @@ function checkUser(token : string) : string | null {
     }catch(e){
         return null;
     }
-    return null;
-    
 }
 
 wss.on('connection', function connection(ws, request){
@@ -53,10 +51,25 @@ wss.on('connection', function connection(ws, request){
     
     ws.on('message', async function message(data){
         const parsedData = JSON.parse(data as unknown as string)
-        if(parsedData.type === "join_room"){
-            const user = users.find(x=>x.ws === ws);
-            user?.rooms.push(parsedData.roomId);
+        if (parsedData.type === "join_room") {
+            const roomId = parsedData.roomId;
+            const room = await prismaClient.room.findUnique({
+            where: { id: roomId }
+        });
+
+        if (!room) {
+            ws.send(JSON.stringify({
+                type: "error",
+                message: `Room with ID ${roomId} does not exist.`
+            }));
+            return;
         }
+
+        const user = users.find(x => x.ws === ws);
+        if (user && !user.rooms.includes(roomId)) {
+            user.rooms.push(roomId);
+    }
+}
         if(parsedData.type === "leave_room"){
             const user = users.find(x => x.ws == ws);
             if(!user){
